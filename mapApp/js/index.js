adeacf144fda090e86779b50677fe2f613bed437
developeron29 <developeron29@gmail.com>
2020-01-10 00:12:45 +0530

    graphs

--- mapApp/js/index.js
+++ mapApp/js/index.js
@@ -36,7 +36,7 @@ window.onload = function () {
     var newHeight = $(window).height();
     $("#mapid").height(newHeight - 15);
 
-    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+    var mymap = L.map('mapid').setView([47.6131746,-122.4821483], 10);
     var CartoDB_DarkMatter = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
         subdomains: 'abcd',
